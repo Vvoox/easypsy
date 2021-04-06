d7d89dd62c565455ed9735c53e86da557e1450ca
Khalil <daoulat.khalil@gmail.com>
2021-04-06 08:55:58 +0100

    Add screen for settings page.

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -1,35 +1,72 @@
 import * as WebBrowser from 'expo-web-browser';
-import React from 'react';
-import { StyleSheet, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+import { Button, TouchableOpacity} from 'react-native';
 
 import Colors from '../constants/Colors';
 import { MonoText } from './StyledText';
 import { Text, View } from './Themed';
+import { Alert, Modal, StyleSheet, TouchableHighlight } from 'react-native';
 
 export default function EditScreenInfo({ path }: { path: string }) {
+  const [modalVisible, setModalVisible] = useState(false);
+  const createThreeButtonAlert = () =>
+      Alert.alert(
+          "Alert Title",
+          "My Alert Msg",
+          [
+            {
+              text: "Ask me later",
+              onPress: () => console.log("Ask me later pressed")
+            },
+            {
+              text: "Cancel",
+              onPress: () => console.log("Cancel Pressed"),
+              style: "cancel"
+            },
+            { text: "OK", onPress: () => console.log("OK Pressed") }
+          ],
+          { cancelable: false }
+      );
   return (
     <View>
       <View style={styles.getStartedContainer}>
-        {/*<Text*/}
-        {/*  style={styles.getStartedText}*/}
-        {/*  lightColor="rgba(0,0,0,0.8)"*/}
-        {/*  darkColor="rgba(255,255,255,0.8)">*/}
-        {/*  Welcome Doctor*/}
-        {/*</Text>*/}
+        <Button title="Add Patient" onPress={createThreeButtonAlert} />
+        <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert('Modal has been closed.');
+            }}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>Hello World!</Text>
 
-        {/*<View*/}
-        {/*  style={[styles.codeHighlightContainer, styles.homeScreenFilename]}*/}
-        {/*  darkColor="rgba(255,255,255,0.05)"*/}
-        {/*  lightColor="rgba(0,0,0,0.05)">*/}
-        {/*  <MonoText>{path}</MonoText>*/}
-        {/*</View>*/}
+              <TouchableHighlight
+                  style={{ ...styles.openButton, backgroundColor: '#2196F3' }}
+                  onPress={() => {
+                    setModalVisible(!modalVisible);
+                  }}>
+                <Text style={styles.textStyle}>Hide Modal</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
 
+        <TouchableHighlight
+            style={styles.openButton}
+            onPress={() => {
+              setModalVisible(true);
+            }}>
+          <Text style={styles.textStyle}>Show Modal</Text>
+        </TouchableHighlight>
         <Text
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
           darkColor="rgba(255,255,255,0.8)">
           Welcome Doctor
         </Text>
+
       </View>
 
       <View style={styles.helpContainer}>
@@ -103,6 +140,42 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
   },
   helpLinkText: {
+    textAlign: 'center',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  openButton: {
+    backgroundColor: '#F194FF',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
     textAlign: 'center',
   },
 });
